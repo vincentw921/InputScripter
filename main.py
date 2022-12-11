23efbf6f5888a9b76587919652bbbc7243080544
vincentw921 <vinowang921@gmail.com>
2022-12-10 21:11:46 -0500

    Add modules

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3 --> #!/usr/bin/python
+
 import keyboard
-import mouse
+# import mouse
 
 recording = False
 
@@ -9,6 +11,6 @@ def on_f6():
 while True:
     if (recording):
         keyboard.record()
-    if (keyboard.is_pressed('f6')):
+    if (keyboard.is_pressed('F6')):
         on_f6()
     
